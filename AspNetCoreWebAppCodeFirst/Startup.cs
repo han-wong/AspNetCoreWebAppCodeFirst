b395503aebc74bb86a391bc2c0b08ab746272999
Han Wong <han.wong@pm.me>
2021-08-23 22:17:28 +0200

    configured container

--- AspNetCoreWebAppCodeFirst/Startup.cs
+++ AspNetCoreWebAppCodeFirst/Startup.cs
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AspNetCoreWebAppCodeFirst.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace AspNetCoreWebAppCodeFirst
 {
@@ -23,6 +25,8 @@ namespace AspNetCoreWebAppCodeFirst
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddRazorPages();
         }
 
